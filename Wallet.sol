26c42a3c28c79d7b18df151332e2c413a36a8f96
Aamir Usmani <usmaniaamir530@gmail.com>
2023-04-21 18:37:12 +0000

    added smart contract

--- Wallet.sol
+++ Wallet.sol
@@ -1,30 +1,31 @@
 pragma ton-solidity >= 0.67.0;
 pragma AbiHeader expire;
 
-contract App {
-    uint32 public timestamp;
+contract Wallet {
+
+    event TokenReceived(address indexed sender, uint256 indexed amount);
+    event TokenPaid(address indexed receiver, uint128 indexed amount);
 
     constructor() {
         require(tvm.pubkey() != 0, 101);
         require(msg.pubkey() == tvm.pubkey(), 102);
         tvm.accept();
-
-        timestamp = block.timestamp;
     }
 
-    function renderHelloWorld () public pure returns (string) {
-        return 'helloWorld';
-    }
-
-    function touch() external {
+    function receiveTokens() external payable {
         require(msg.pubkey() == tvm.pubkey(), 102);
         tvm.accept();
-        timestamp = block.timestamp;
+        emit TokenReceived(msg.sender, msg.value);
     }
 
     function sendValue(address dest, uint128 amount, bool bounce) public view {
         require(msg.pubkey() == tvm.pubkey(), 102);
         tvm.accept();
         dest.transfer(amount, bounce, 0);
+        emit TokenPaid(dest, amount);
+    }
+
+    function getBalance() public returns(uint256){
+        return address(this).balance;
     }
 }
