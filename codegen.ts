cee14c9a0f433d395bd3bbc59c0de49b1f6e0f0b
Aamir Usmani <usmaniaamir530@gmail.com>
2023-04-25 13:49:30 +0000

    final commit

--- codegen.ts
+++ codegen.ts
@@ -1,16 +1,17 @@
-import { CodegenConfig } from '@graphql-codegen/cli'
- 
+import { CodegenConfig } from '@graphql-codegen/cli';
+
 const config: CodegenConfig = {
+  overwrite: true,
   schema: 'https://devnet.evercloud.dev/223ed117967a44f9ad7a5f01f67d04da/graphql',
-  documents: ['./Queries.ts', "./Mutations.ts"],
-  emitLegacyCommonJSImports: false,
-  ignoreNoDocuments: true,
+  documents: './Queries.ts',
   generates: {
-    './src/gql/': {
-      preset: 'client',
-      plugins: ["typescript", "typescript-operations"]
-    }
-  }
-}
- 
-export default config
+    'src/generated/graphql.ts': {
+      plugins: [
+        'typescript',
+        'typescript-operations',
+      ],
+    },
+  },
+};
+
+export default config;
--- src/index2.ts
+++ src/index2.ts
@@ -1,10 +1,16 @@
-import { graphql } from './gql/gql';
 import { GET_TRANSACTION_DATA } from '../Queries';
-import { GetTransactionDataDocument, GetTransactionDataQuery } from './gql/graphql';
+import {request} from 'graphql-request'
+import {GetTransactionDataQuery} from "./generated/graphql";
   const url = "https://devnet.evercloud.dev/223ed117967a44f9ad7a5f01f67d04da/graphql"
 
   async function main() {
-    console.log(graphql)
+    request<GetTransactionDataQuery>(url, GET_TRANSACTION_DATA,{},{
+      "X-API-KEY": `f6d4f83a3dcc4496926af64e371384e8`
+    }).then(data=>{
+      console.log(data.blockchain?.account?.transactions?.edges);
+    }).catch(err=>{
+      console.log(err);
+    })
   } 
 
   main()
